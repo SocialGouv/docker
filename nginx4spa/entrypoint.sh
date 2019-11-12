1d901828152980ef676b5a60c9728c57faa35ccc
Douglas Duteil <douglasduteil@gmail.com>
2019-11-12 21:43:13 +0100

    wip

--- nginx4spa/entrypoint.sh
+++ nginx4spa/entrypoint.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env sh
 
-WWW_DIRECTORY=${WWW_DIRECTORY:="/usr/share/nginx/html"}
-GLOB=${GLOB:="*"}
-
-##############################################################
-# Step 1 : replace default environment values in .html files #
-##############################################################
-
-# Save env variable in file
-printenv >> env-vars
-
-while IFS='=' read -r KEY VALUE
-do
-  # replace default environment variables value
-  find ${WWW_DIRECTORY} -type f -exec sed -i -e "s|%%${KEY}%%|${VALUE}|g" {} \;
-done <env-vars
-
-rm env-vars
-
-#########################
-# Step 2 : Start NGINX  #
-#########################
+source /envsub.sh
 
 exec nginx -g 'daemon off;'
---
+++ nginx4spa/envsub.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+################################################################################
+# Replace environment values in files                                          #
+################################################################################
+
+#
+# As defualt, in every files in the "/usr/share/nginx/html" directory, this
+# script replace `%%KEY%%` by `VALUE` where `export KEY=VALUE` in the global env var.
+#
+
+WWW_DIRECTORY=${WWW_DIRECTORY:="/usr/share/nginx/html"}
+DELIMITER=${DELIMITER:="%%"}
+
+# Save env variable in file
+printenv > /tmp/env-vars
+
+while IFS='=' read -r KEY VALUE
+do
+  # In every files in the dir, replace the environment variables value
+  find ${WWW_DIRECTORY} -type f -exec \
+    sed -i -e "s|${DELIMITER}${KEY}${DELIMITER}|${VALUE}|g" {} \;
+done < /tmp/env-vars
+
+rm /tmp/env-vars
