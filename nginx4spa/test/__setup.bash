bd1219a6ef8d953f5eafd2d68c99412db0a8b581
Douglas Duteil <douglasduteil@gmail.com>
2021-02-23 18:44:47 +0100

    ci(github): github workflow for nginx4spa (#540)

--- nginx4spa/test/__setup.bash
+++ nginx4spa/test/__setup.bash
@@ -3,15 +3,5 @@
 load /usr/lib/bats-support/load.bash
 load /usr/lib/bats-assert/load.bash
 
-PROJECT_DIR=${CI_PROJECT_DIR:="$(pwd)"}
-REGISTRY_IMAGE=${CI_REGISTRY_IMAGE:="registry.localhost/socialgouv/docker"}
-IMAGE_TAG=${CI_COMMIT_SHA:="latest"}
-
-if [[ -n $CI ]]; then
-  return 0
-fi
-
-# echo "Run"
-# echo "docker build nginx4spa -t ${REGISTRY_IMAGE}/nginx4spa:${IMAGE_TAG}"
-# echo "docker build nginx4spa -t registry.localhost/socialgouv/docker/nginx4spa:latest"
-# echo ""
+REGISTRY_IMAGE=${REGISTRY_IMAGE:="registry.localhost/socialgouv/docker"}
+IMAGE_TAG=${IMAGE_TAG:="latest"}
